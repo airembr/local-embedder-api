f71cc73738363d7aa7ec7902f524c04ab5d43216
risto <risto.kowaczewski@gmail.com>
2025-10-27 10:45:34 +0100

    Add BM25 sparse embeddings and update dependencies

--- app/routes/embedder_endpoint.py
+++ app/routes/embedder_endpoint.py
@@ -1,10 +1,11 @@
 import logging
+from time import time
 from typing import List
 
 from fastapi import APIRouter, Depends
 
 from app.config import model
-from app.service.embedder import get_embeddings
+from app.service.embedder import get_embeddings, get_bm25
 from app.service.security import verify_token
 
 logger = logging.getLogger(__name__)
@@ -14,6 +15,11 @@ router = APIRouter(dependencies=[Depends(verify_token)])
 
 
 @router.post("/embeddings")
-async def embeddings(sentences: List[str]):
-    result, elapsed = get_embeddings(sentences)
-    return {"embeddings": result.tolist(), "model": model, "elapsed": elapsed}
+async def embeddings(sentences: List[str], bm25: bool = False):
+    t = time()
+    embeddings = get_embeddings(sentences)
+    if bm25:
+        bm25 = get_bm25(sentences)
+    else:
+        bm25 = None
+    return {"embeddings": {"dense": embeddings.tolist(), "sparse": bm25}, "model": model, "elapsed": time() - t}
--- app/service/embedder.py
+++ app/service/embedder.py
@@ -1,14 +1,15 @@
-from time import time
+from typing import List
 
 from sentence_transformers import SentenceTransformer
-
+from fastembed.sparse.bm25 import Bm25
 from app.config import model
 
 dense_embedder = SentenceTransformer(model)
+bm25_embedder = Bm25('Qdrant/bm25')
+
+def get_embeddings(sentences: List[str]):
+    return dense_embedder.encode(sentences, show_progress_bar=False)
 
 
-def get_embeddings(sentences):
-    t = time()
-    embeddings = dense_embedder.encode(sentences, show_progress_bar=False)
-    elapsed = time() - t
-    return embeddings, elapsed
+def get_bm25(sentences: List[str]):
+    return bm25_embedder.passage_embed(sentences)
