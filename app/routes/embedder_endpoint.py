8b8679fee8d00ba6ca9889855645fcfd16186cc1
risto <risto.kowaczewski@gmail.com>
2025-11-18 11:41:28 +0100

    Update embeddings endpoint to process OrderedDict inputs and return structured response

--- app/routes/embedder_endpoint.py
+++ app/routes/embedder_endpoint.py
@@ -1,12 +1,15 @@
 import logging
+from collections import OrderedDict
 from time import time
-from typing import List
+from typing import List, Dict
 
 from fastapi import APIRouter, Depends
 
 from app.config import model
 from app.service.embedder import get_embeddings, get_bm25
 from app.service.security import verify_token
+
+from sdk.airembr.model.embedding.embedding import EmbeddingResponse
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
@@ -21,14 +24,27 @@ def _convert(bm25):
         item['indices'] = item['indices'].tolist()
         yield item
 
+
 @router.post("/embeddings")
-async def embeddings(sentences: List[str], bm25: bool = False):
+async def embeddings(texts: OrderedDict[str, str], bm25: bool = False):
     t = time()
-    embeddings = get_embeddings(sentences)
+
+    relations = list(texts.keys())
+    values = list(texts.values())
+
+    embeddings = get_embeddings(values)
+    embeddings = embeddings.tolist()
+
+    if embeddings:
+        embeddings = list(zip(relations, embeddings))
+
     if bm25:
-        bm25 = get_bm25(sentences)
+        bm25 = get_bm25(values)
         bm25 = list(list(_convert(bm25)))
+        bm25 = list(zip(relations, bm25))
     else:
         bm25 = None
+    elapsed = time() - t
 
-    return {"embeddings": {"sparse": bm25, "dense": embeddings.tolist()}, "model": model, "elapsed": time() - t}
+    logger.info(f"Vectors: {len(embeddings)}, Elapsed time: {elapsed}")
+    return EmbeddingResponse(sparse=bm25, dense=embeddings, model=model, elapsed=elapsed)
