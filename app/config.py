008d2bc2b3e8fde1023039bf457aba5d8bcbc403
risto <risto.kowaczewski@gmail.com>
2026-02-27 10:20:46 +0100

    Make `model` configurable via `EMBEDDING_MODEL` and add environment utility functions

--- app/config.py
+++ app/config.py
@@ -1,3 +1,5 @@
+import os
+
 # model = 'paraphrase-multilingual-MiniLM-L12-v2'
-model = 'intfloat/multilingual-e5-base'
+model = os.environ.get("EMBEDDING_MODEL", 'intfloat/multilingual-e5-base')
 # model = 'BAAI/bge-m3'
